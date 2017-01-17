4633d92037dee5d538db02b4fa0a04eb71a7308e
Smerth <merth@me.com>
2017-01-17 14:28:21 -0800

    End Chapter 01-06 Serve JSON data.

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
 
-app.get('/', function(request, response){
-    response.send('<h1>Chat That Word</h1>')
+app.set('port', process.env.PORT || 3000 );
+
+app.get('/', function(req, res) {
+  var info = '';
+  dataFile.speakers.forEach(function(item) {
+    info += `
+    <li>
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+    </li>
+    `;
+  });
+  res.send(`
+      <h1>Roux Academy Meetups</h1>
+      ${info}
+  `);
 });
 
-var server = app.listen(3000, function(){
-    console.log('Listening on port: 3000...')
-})
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
 
 
 // CHAPTER 01-04 NODE SERVER
