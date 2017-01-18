92b3f276d8c4a8d82bb21d74c6bc0f432ff3a013
Smerth <merth@me.com>
2017-01-17 21:01:09 -0800

    Refactored the app. Placed all app files in an app folder. Changed all paths and scripts to include app folder. Now scripts in package.json can target app folder.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var app = express();
 var reload = require('reload');
+var app = express();
 var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
 
-app.use(express.static('public'));
+app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 
